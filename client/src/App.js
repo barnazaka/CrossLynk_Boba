87ceaca0c1b94e3553eb01e65d6082025377fcab
Barnabas Zakariya <90233177+barnazaka@users.noreply.github.com>
2024-10-21 19:36:41 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AssetManager from './components/AssetManager';
+
+const App = () => {
+    return (
+        <div>
+            <h1>CrossLynk_Boba</h1>
+            <AssetManager />
+        </div>
+    );
+};
+
+export default App;
