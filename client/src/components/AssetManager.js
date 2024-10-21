3f89e5f1eee6a09cea32266c0f857f8e3b88137a
Barnabas Zakariya <90233177+barnazaka@users.noreply.github.com>
2024-10-21 19:37:14 +0530

    Create AssetManager.js

---
+++ client/src/components/AssetManager.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TransferForm from './TransferForm';
+
+const AssetManager = () => {
+    return (
+        <div>
+            <h2>Manage Your Assets</h2>
+            <TransferForm />
+        </div>
+    );
+};
+
+export default AssetManager;
